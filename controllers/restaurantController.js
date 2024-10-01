f0820f8410c195328382b50514d1e65d5a632b59
g pooja <pooja.g7277@gmail.com>
2024-10-01 05:56:14 +0000

    backend CRUD done

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -25,3 +25,41 @@ exports.getAllRestaurant = async (req, res) => {
         res.status(400).send(err.message); // Send an error response if something  wrong
     }
 };
+// Get a room by ID
+exports.getRestaurantById = async (req, res) => {
+    try {
+        const restaurantById = await RestaurantModel.findById(req.params.id); // Find room by ID
+        if (!restaurantById) return res.status(404).send('Restaurant not found in database'); // If room is not found, return 404
+        res.send(restaurantById); // Send the room as a response
+    } catch (err) {
+        res.status(400).send(err.message); // Send an error response if something goes wrong
+    }
+};
+
+// Update a room by ID
+exports.updateRestaurant = async (req, res) => {
+    try {
+        const updatedRestaurant = await RestaurantModel.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            phonenumber: req.body.phonenumber,
+            location: req.body.location,
+            ratings: req.body.ratings
+        }, { new: true }); // Return the updated room
+
+        if (!updatedRestaurant) return res.status(404).send('Restaurant not found in database'); // If room is not found, return 404
+        res.send(updatedRestaurant); // Send the updated room as a response
+    } catch (err) {
+        res.status(400).send(err.message); // Send an error response if something goes wrong
+    }
+};
+
+// Delete a room by ID
+exports.deleteRestaurant = async (req, res) => {
+    try {
+        const restaurantById = await RestaurantModel.findByIdAndDelete(req.params.id); // Find room by ID and delete it
+        if (!restaurantById) return res.status(404).send('Restaurant not found in database'); // If room is not found, return 404
+        res.send("Restaurant deleted successfully"); // Send success message
+    } catch (err) {
+        res.status(400).send(err.message); // Send an error response if something goes wrong
+    }
+};
--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -5,5 +5,8 @@ const restaurantController = require('../controllers/restaurantController.js');
 // Routes for Room CRUD operations
 router.post('/restaurant', restaurantController.createRestaurant);      // Create a new reastaurant
 router.get('/restaurant', restaurantController.getAllRestaurant);      // Get all restaurants
+router.get('/restaurant/:id', restaurantController.getRestaurantById);  // Get a single restaurant by ID
+router.put('/restaurant/:id', restaurantController.updateRestaurant);   // Update a restaurant by ID
+router.delete('/restaurant/:id', restaurantController.deleteRestaurant);// Delete a restaurant by ID
 
 module.exports = router;
