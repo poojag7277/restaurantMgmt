c75f81d665fbb1c2b45f338a4f0a49d00b66e027
g pooja <pooja.g7277@gmail.com>
2024-12-07 16:48:54 +0000

    navbar done

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,9 +10,9 @@ import {
   Menu,
   MenuItem,
 } from '@mui/material';
-//import GitHubIcon from '@mui/icons-material/GitHub';
-import HomeIcon from '@mui/icons-material';
-import MenuBookIcon from '@mui/icons-material'; // Added Notes icon
+import GitHubIcon from '@mui/icons-material/GitHub';
+import HomeIcon from '@mui/icons-material/Home';
+import MenuBookIcon from '@mui/icons-material/MenuBook'; // Added Notes icon
 
 const notesPages = [
     { title: 'Home', path: '/notes/home' },
@@ -23,7 +23,7 @@ const notesPages = [
 const Navbar= () => {
     const [notesAnchorEl, setNotesAnchorEl] = useState(null);
 
-        const handleNoteslick = (event) =>{
+        const handleNotesClick = (event) =>{
             setNotesAnchorEl(event.currentTarget);
         }
     
@@ -33,22 +33,49 @@ const Navbar= () => {
 
           return (
             <AppBar  position="static" color="transparent" elevation={0} sx={{ width: '100%' }} >
-
                 <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'primary.main' }}>
-               Project
-             </Typography>
-        
+               Restaurant Management Project
+                </Typography>
                     <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                        <Button color='red' >
+                        <Button 
+                        color="primary"
+                        component={RouterLink}
+                        to="/"
+                        startIcon={<HomeIcon />} >
                             HomePage
                         </Button>
-                        <Button color='red' >
+                        <Button  
+                        color="primary"
+                        onClick={handleNotesClick}
+                        startIcon={<MenuBookIcon />} // Added icon here
+             >
                             Popular Items
                         </Button>
-                        <Menu>
+                        <Menu 
+                        anchorEl={notesAnchorEl}
+            open={Boolean(notesAnchorEl)}
+            onClose={handleNotesClose}
+            >
+              {notesPages.map((page) => (
+                <MenuItem 
+                key={page.path}
+                component={RouterLink}
+                to={page.path}
+                onClick={handleNotesClose}
+                >
+                  {page.title}
+                </MenuItem>
+              ))}
                         </Menu>
-                        <IconButton>
+                        <IconButton 
+                        color="primary"
+                        component="a"
+                        href="https://github.com/poojag7277/restaurantMgmt"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Github">
+                          < GitHubIcon />
                         </IconButton>
                     </Box>
                 </Toolbar>
